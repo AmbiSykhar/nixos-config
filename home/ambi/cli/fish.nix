3cfaef3c5fed4fcdb1fdd451f5ee638c31b0b0f5
Ambi <github@alayna.dev>
2026-02-28 14:12:07 -0500

    Remove fish greeting message

--- home/ambi/cli/fish.nix
+++ home/ambi/cli/fish.nix
@@ -1,8 +1,11 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   programs.fish = {
     enable = true;
+    interactiveShellInit = ''
+      set fish_greeting
+    '';
     plugins = [
       { name = "tide"; src = pkgs.fishPlugins.tide.src; }
     ];
