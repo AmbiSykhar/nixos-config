9029fdf54cc7945213b1b27363cf18b0da574815
Ambi <github@alayna.dev>
2026-03-12 23:48:49 -0400

    Add gnome.nix

---
+++ hosts/common/optional/gnome.nix
@@ -0,0 +1,16 @@
+{
+  services = {
+    xserver = {
+      desktopManager.gnome = {
+        enable = true;
+      };
+      displayManager.gdm = {
+        enable = true;
+        autoSuspend = false;
+      };
+    };
+    gnome.games.enable = true;
+  };
+  services.avahi.enable = false;
+  networking.networkmanager.enable = false;
+}
